8a60beb6033c597da724ce1382ec89f1f660f0ac
Vladimir Stoilov <vladimir@safing.io>
2024-03-22 10:51:24 +0200

    Fix wierd crash

--- driver/src/id_cache.rs
+++ driver/src/id_cache.rs
@@ -1,5 +1,5 @@
 use alloc::collections::VecDeque;
-use wdk::rw_spin_lock::RwMutex;
+use wdk::rw_spin_lock::RwSpinLock;
 
 struct Entry<T> {
     value: T,
@@ -7,19 +7,21 @@ struct Entry<T> {
 }
 
 pub struct IdCache<T> {
-    values: Option<RwMutex<VecDeque<Entry<T>>>>,
+    values: Option<VecDeque<Entry<T>>>,
+    lock: RwSpinLock,
     next_id: u64,
 }
 
 impl<T> IdCache<T> {
     pub fn init(&mut self) {
-        self.values = Some(RwMutex::new(VecDeque::with_capacity(1000)));
+        self.values = Some(VecDeque::with_capacity(1000));
+        self.lock = RwSpinLock::default();
         self.next_id = 1; // 0 is invalid id
     }
 
     pub fn push(&mut self, value: T) -> u64 {
         if let Some(values) = &mut self.values {
-            let mut values = values.lock_mut();
+            let _guard = self.lock.write_lock();
             let id = self.next_id;
             values.push_back(Entry { value, id });
             self.next_id = self.next_id.wrapping_add(1); // Assuming this will not overflow.
@@ -30,7 +32,7 @@ impl<T> IdCache<T> {
 
     pub fn pop_id(&mut self, id: u64) -> Option<T> {
         if let Some(values) = &mut self.values {
-            let mut values = values.lock_mut();
+            let _guard = self.lock.write_lock();
             if let Ok(index) = values.binary_search_by_key(&id, |val| val.id) {
                 return Some(values.remove(index).unwrap().value);
             }
@@ -40,7 +42,7 @@ impl<T> IdCache<T> {
 
     pub fn get_entries_count(&self) -> usize {
         if let Some(values) = &self.values {
-            let values = values.lock();
+            let _guard = self.lock.read_lock();
             return values.len();
         }
 
--- wdk/src/rw_spin_lock.rs
+++ wdk/src/rw_spin_lock.rs
@@ -8,6 +8,54 @@ use windows_sys::Wdk::System::SystemServices::{
     ExReleaseSpinLockShared,
 };
 
+pub struct RwSpinLock {
+    data: UnsafeCell<i32>,
+}
+
+impl RwSpinLock {
+    pub const fn default() -> Self {
+        Self {
+            data: UnsafeCell::new(0),
+        }
+    }
+
+    pub fn read_lock(&self) -> RwLockGuard {
+        let irq = unsafe { ExAcquireSpinLockShared(self.data.get()) };
+        RwLockGuard {
+            data: &self.data,
+            exclusive: false,
+            old_irq: irq,
+        }
+    }
+
+    pub fn write_lock(&self) -> RwLockGuard {
+        let irq = unsafe { ExAcquireSpinLockExclusive(self.data.get()) };
+        RwLockGuard {
+            data: &self.data,
+            exclusive: true,
+            old_irq: irq,
+        }
+    }
+}
+
+pub struct RwLockGuard<'a> {
+    data: &'a UnsafeCell<i32>,
+    exclusive: bool,
+    old_irq: u8,
+}
+
+impl<'a> Drop for RwLockGuard<'a> {
+    fn drop(&mut self) {
+        unsafe {
+            if self.exclusive {
+                ExReleaseSpinLockExclusive(self.data.get(), self.old_irq);
+            } else {
+                ExReleaseSpinLockShared(self.data.get(), self.old_irq);
+            }
+        }
+    }
+}
+
 pub struct RwMutex<T> {
     lock: UnsafeCell<i32>,
     data: T,
